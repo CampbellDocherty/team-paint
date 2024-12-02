f6f97e4defb9324da8d23235c09503d1eabf8bfa
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2024-12-02 12:49:45 +0000

    P5 and ml5 attempt

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import type { ViteUserConfig as VitestUserConfigInterface } from 'vitest/config';
+import { viteExternalsPlugin } from 'vite-plugin-externals'
 
 const vitestConfig: VitestUserConfigInterface = {
   test: {
@@ -11,6 +12,10 @@ const vitestConfig: VitestUserConfigInterface = {
 
 export default defineConfig({
   base: "./",
-  plugins: [react()],
+  plugins: [
+    viteExternalsPlugin({
+      ml5: '/public/libraries/ml5.min.js',
+    }),
+    react()],
   ...vitestConfig
 })
