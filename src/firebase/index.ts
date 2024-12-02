492bc14091f45be978043fbdae243b2f6934a8cb
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2024-12-02 11:52:14 +0000

    Add firebase

---
+++ src/firebase/index.ts
@@ -0,0 +1,46 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getStorage,
+  listAll,
+  ref,
+  getDownloadURL,
+  getMetadata,
+  FullMetadata,
+} from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyC2FGC8poUDlYS7QMDOaTZ4Cx9mDqXCRis',
+  authDomain: 'team-paint.firebaseapp.com',
+  projectId: 'team-paint',
+  storageBucket: 'team-paint.firebasestorage.app',
+  messagingSenderId: '166674041451',
+  appId: '1:166674041451:web:d4498a144e363602283f2e',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+const storage = getStorage();
+
+// Create a reference under which you want to list
+const listRef = ref(storage, `uploads/`);
+
+export type FirebaseStorageContent = {
+  downloadUrl: string;
+  metadata: FullMetadata;
+};
+
+export const getFiles = async () => {
+  const res = await listAll(listRef);
+  const promisedFiles = res.items.map(async (item) => {
+    const storageRef = ref(storage, item.fullPath);
+    const downloadUrl = await getDownloadURL(storageRef);
+    const metadata = await getMetadata(storageRef);
+    return {
+      downloadUrl,
+      metadata,
+    };
+  });
+  const files: FirebaseStorageContent[] = await Promise.all(promisedFiles);
+  return files;
+};
