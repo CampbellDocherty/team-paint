426a292fc5aff9207bec8f8a18a4e3bfb785584b
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2024-12-02 11:59:01 +0000

    Title

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { styled } from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  height: ${innerHeight}px;
+  display: flex;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+  font-family: 'Inconsolata';
+  font-size: 20px;
+`;
+
 const App = () => {
-  return <p>Team Paint</p>;
+  return (
+    <Container>
+      <Title>Team Paint</Title>
+    </Container>
+  );
 };
 
 export default App;
