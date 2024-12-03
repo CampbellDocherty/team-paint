25b1bf5b9d3fddd9df242d868f64b0e18e01f7b4
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2024-12-03 10:47:28 +0000

    Split screen for colours

--- src/NewGame.tsx
+++ src/NewGame.tsx
@@ -4,9 +4,9 @@ import { styled } from 'styled-components';
 
 const Container = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   height: ${innerHeight}px;
-  gap: 4px;
+  gap: 24px;
   padding: 8px;
 `;
 
