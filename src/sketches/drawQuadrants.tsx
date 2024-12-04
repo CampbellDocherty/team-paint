5cfc19d863e95adacc818d3c1c0647d8664027bf
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2024-12-04 11:18:28 +0000

    Ensure stroke on quadrant

--- src/sketches/drawQuadrants.tsx
+++ src/sketches/drawQuadrants.tsx
@@ -6,23 +6,20 @@ export function drawQuadrants(
 ) {
   sketch.push();
   sketch.strokeWeight(1);
-  sketch.push();
+  sketch.stroke('black');
   sketch.fill('red');
   sketch.circle(20, 20, 20);
-  sketch.pop();
-  sketch.push();
+
   sketch.fill('green');
   sketch.circle(sketch.width / 2 - 20, 20, 20);
-  sketch.pop();
-  sketch.push();
+
   sketch.fill('blue');
   sketch.circle(20, sketch.height - 20, 20);
-  sketch.pop();
-  sketch.push();
+
   const { r, g, b } = currentColour;
   sketch.fill(r, g, b);
   sketch.circle(sketch.width / 2 - 20, sketch.height - 20, 20);
-  sketch.pop();
+
   sketch.line(sketch.width / 4, 0, sketch.width / 4, sketch.height);
   sketch.line(0, sketch.height / 2, sketch.width / 2, sketch.height / 2);
   sketch.pop();
