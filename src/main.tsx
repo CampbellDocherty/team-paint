cdb36219df8a1f58418cf34d878e7c28e513ed50
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2024-12-04 11:36:43 +0000

    Refactor

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router';
-import App from './App';
+import Home from './pages/Home';
 import './index.css';
-import { Casual } from './NewGame';
-import { TeamName } from './TeamName';
+import { Casual } from './pages/Casual';
+import { TeamName } from './pages/TeamName';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
         <Route path="/name" element={<TeamName />} />
         <Route path="/casual" element={<Casual />} />
       </Routes>
---
+++ src/pages/Casual.tsx
@@ -0,0 +1,20 @@
+import { ReactP5Wrapper } from '@p5-wrapper/react';
+import { styled } from 'styled-components';
+import { ColourSelect } from '../sketches/ColourSelect';
+import { Container, Title, Button } from './styles';
+
+const SketchWrapper = styled.div`
+  margin: 30px 0px;
+`;
+
+export const Casual = () => {
+  return (
+    <Container>
+      <Title>Casual</Title>
+      <SketchWrapper>
+        <ReactP5Wrapper sketch={ColourSelect} />
+      </SketchWrapper>
+      <Button className="save-image">Save</Button>
+    </Container>
+  );
+};
