c7174d5021a719a3a12055f3d391e56e06395386
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2024-12-05 14:37:38 +0000

    HAsh router

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { HashRouter, Route, Routes } from 'react-router';
 import Home from './pages/Home';
 import './index.css';
 import { Casual } from './pages/Casual';
@@ -8,12 +8,12 @@ import { TeamName } from './pages/TeamName';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename={location.pathname}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/name" element={<TeamName />} />
         <Route path="/casual" element={<Casual />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
